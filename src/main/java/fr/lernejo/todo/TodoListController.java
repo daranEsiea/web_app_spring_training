66a8774130112f74db421dd272cdce60a0923c3f
daranEsiea <vignarajah@et.esiea.fr>
2021-12-06 14:44:26 +0100

    Partie 4 filtre header HTTP

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -4,22 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class TodoListController {
+    private final static List<Todo> todos = new ArrayList<Todo>();
 
-    private static List<Todo> todos = new ArrayList<Todo>();
-
-    @PostMapping(value = "/api/todo")
-    public ResponseEntity<Todo> addTodo(Todo todo){
-        todos.add(todo);
-        return new ResponseEntity<Todo>(todo, HttpStatus.CREATED);
+    @GetMapping("/api/todo")
+    public ResponseEntity<List<Todo>> getTodos() {
+        return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/api/todo")
-    public ResponseEntity<List<Todo>> getTodos(){
-        return new ResponseEntity<List<Todo>>(todos, HttpStatus.CREATED);
+    @PostMapping(value = "/api/todo", consumes = {"application/json"})
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
+        todos.add(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.CREATED);
     }
 }
